6b6198bc7ecb71678f4b8c9c354c9242b7c5720d
unknown <vinodbgavali@gmail.com>
2020-10-11 03:58:58 +0530

    deploy new website to github

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './index.css';
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="app">
+      <h1 >New Website</h1>
+    </div>
+  );
+}
+
+export default App;
