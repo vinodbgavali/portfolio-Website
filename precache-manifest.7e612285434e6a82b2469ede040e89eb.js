6fad9384e4a00c6117a440fafb3fcf91b816fb4a
unknown <vinodbgavali@gmail.com>
2020-10-11 03:59:39 +0530

    Updates

---
+++ precache-manifest.7e612285434e6a82b2469ede040e89eb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b5e1c24d047780160affca0a5941e9f",
+    "url": "/vinodbgavali/portfolio-Website/index.html"
+  },
+  {
+    "revision": "06ecded33928f261db59",
+    "url": "/vinodbgavali/portfolio-Website/static/css/main.cb2386ba.chunk.css"
+  },
+  {
+    "revision": "5a95a1913bac3b57d007",
+    "url": "/vinodbgavali/portfolio-Website/static/js/2.6fb2d1bf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/vinodbgavali/portfolio-Website/static/js/2.6fb2d1bf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "06ecded33928f261db59",
+    "url": "/vinodbgavali/portfolio-Website/static/js/main.ae33121c.chunk.js"
+  },
+  {
+    "revision": "083f4f12a1e84e4e8c64",
+    "url": "/vinodbgavali/portfolio-Website/static/js/runtime-main.32540d2b.js"
+  }
+]);
---
+++ static/js/main.ae33121c.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpnitin-portfolio"]=this["webpackJsonpnitin-portfolio"]||[]).push([[0],[,,function(e,n,t){},,function(e,n,t){e.exports=t(10)},,,,,function(e,n,t){},function(e,n,t){"use strict";t.r(n);var o=t(0),i=t.n(o),r=t(3),a=t.n(r);t(2),t(9);var c=function(){return i.a.createElement("div",{className:"app"},i.a.createElement("h1",null,"New Website"))};a.a.render(i.a.createElement(c,null),document.getElementById("root"))}],[[4,1,2]]]);
+//# sourceMappingURL=main.ae33121c.chunk.js.map
